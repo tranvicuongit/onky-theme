7080647878e599ab1767a35b9b10771c85933e45
Vi Cuong <tranvicuongit@gmail.com>
2017-12-04 18:03:52 +0700

    responsive

--- app/app.js
+++ app/app.js
@@ -71,12 +71,19 @@ var app = angular.module('OnkyApp', ['ngRoute'])
           controller: 'NewsCtrl',
           controllerAs: 'news'
         })
+        .when('/detail-products', {
+          templateUrl: 'app/pages/detail-product.html',
+          controller: 'ProductCtrl',
+          controllerAs: 'pro'
+        })
         .when('/contact', {
           templateUrl: 'app/pages/contact.html',
           controller: 'ContactCtrl',
           controllerAs: 'contact'
         })
-        .otherwise({redirectTo: '/'});
+        .otherwise({
+          redirectTo: '/'
+        });
 
       $locationProvider.html5Mode(true);
     }
@@ -133,11 +140,11 @@ var app = angular.module('OnkyApp', ['ngRoute'])
         $(this).find(".header__menu_item--submenu").css("display", "none");
       });
 
-      $(".products__list--item").mouseenter(function () {
-        $(this).find(".products__list--item_actions").css("opacity", "1");
-      }).mouseleave(function () {
-        $(this).find(".products__list--item_actions").css("opacity", "0");
-      });
+      // $(".products__list--item").mouseenter(function () {
+      //   $(this).find(".products__list--item_actions").css("opacity", "1");
+      // }).mouseleave(function () {
+      //   $(this).find(".products__list--item_actions").css("opacity", "0");
+      // });
 
 
       $("html, body").click(function (e) {
@@ -149,10 +156,69 @@ var app = angular.module('OnkyApp', ['ngRoute'])
         }
       });
 
+
+      $(".header").sticky({});
+      $('.quickview-slider .owl-carousel').owlCarousel({
+        items: 3,
+        navigation: true,
+        navigationText: ['', '']
+      });
+
+      $(function () {
+        $('#tab-detailpro a:first').tab('show');
+        $('#tab-detailpro a').click(function (e) {
+          e.preventDefault()
+          $(this).tab('show')
+        })
+      });
+      $('input.rating[type=number]').each(function () {
+        $(this).rating();
+      });
+
+      $('.quickview-slider .owl-carousel .owl-item').first().children('.product-thumb').addClass('active');
+    }, 2000);
+    //////////////////////////////
+
+    // Create product list
+    $rootScope.Products = [];
+    for (var i = 1; i <= 12; i++) {
+      $rootScope.Products.push({
+        Title: "Product 000" + i,
+        Price: i + "0,000,000",
+        Link: "san-pham-000" + i,
+        Compare: i + "0,000"
+      });
+    }
+
+
+
+    // $("html, body").click(function (e) {
+    //   if ($(e.target).hasClass('cartpreview') && !$(e.target).hasClass("cartpreview__close") && !$('.cartpreview').hasClass("cartpreview--opended")) {
+    //     return false;
+    //   }
+    //   //onkyApp.cart.closePreview();
+    // });​
+
+  });
+
+app.filter('range', function () {
+  return function (input, total) {
+    total = parseInt(total);
+
+    for (var i = 0; i < total; i++) {
+      input.push(i);
+    }
+
+    return input;
+  };
+});
+
+app.controller('HomeCtrl', function HomeController($scope) {
+    setTimeout(function () {
       if ($('.homeslide li').size() > 0) {
         $(".homeslide").owlCarousel({
           singleItem: true,
-          autoPlay: 5000,
+          autoPlay: false,
           items: 1,
           itemsDesktop: [1199, 1],
           itemsDesktopSmall: [980, 1],
@@ -214,153 +280,123 @@ var app = angular.module('OnkyApp', ['ngRoute'])
           // afterInit: nextslide
         });
       }
-        $(".header").sticky({  });
-    }, 2000);
-    //////////////////////////////
+    }, 300);
 
-    // Create product list
-    $rootScope.Products = [];
-
-    for (var i = 1; i <= 12; i++) {
-      $rootScope.Products.push({
-        Title: "Product 000" + i,
-        Price: i + "0,000,000",
-        Link: "san-pham-000" + i,
-        Compare: i + "0,000"
+  })
+  .controller('CartCtrl', function CartController($scope) {})
+  .controller('NewsCtrl', function NewsController($scope) {})
+  .controller('PaymentCtrl', function PaymentController($scope) {})
+  .controller('ContactCtrl', function ContactController($scope) {})
+  .controller('ProductCtrl', function ContactController($scope) {
+    setTimeout(function () {
+      $("#sliderproduct .slides").owlCarousel({
+        items: 3,
+        navigation: true,
+        navigationText: ['', '']
       });
-    }
-
-
-
-    // $("html, body").click(function (e) {
-    //   if ($(e.target).hasClass('cartpreview') && !$(e.target).hasClass("cartpreview__close") && !$('.cartpreview').hasClass("cartpreview--opended")) {
-    //     return false;
-    //   }
-    //   //onkyApp.cart.closePreview();
-    // });​
-
+    }, 300);
   });
-
-  app.filter('range', function() {
-    return function(input, total) {
-      total = parseInt(total);
-  
-      for (var i=0; i<total; i++) {
-        input.push(i);
-      }
-  
-      return input;
-    };
-  });
-
-app.controller('HomeCtrl', function HomeController($scope) {})
-.controller('CartCtrl', function CartController($scope) {})
-.controller('NewsCtrl', function NewsController($scope) {})
-.controller('PaymentCtrl', function PaymentController($scope) {})
-.controller('ContactCtrl', function ContactController($scope) {
-});
 
 
 
 ///////////////////  Sticky /////////////////////
-(function($) {
+(function ($) {
   var defaults = {
-          topSpacing: 0,
-          bottomSpacing: 0,
-          className: 'is-sticky',
-          wrapperClassName: 'sticky-wrapper'
-      },
-      $window = $(window),
-      $document = $(document),
-      sticked = [],
-      windowHeight = $window.height(),
-      scroller = function() {
-          var scrollTop = $window.scrollTop(),
-              documentHeight = $document.height(),
-              dwh = documentHeight - windowHeight,
-              extra = (scrollTop > dwh) ? dwh - scrollTop : 0;
-          for (var i = 0; i < sticked.length; i++) {
-              var s = sticked[i],
-                  elementTop = s.stickyWrapper.offset().top,
-                  etse = elementTop - s.topSpacing - extra;
-              if (scrollTop <= etse) {
-                  if (s.currentTop !== null) {
-                      s.stickyElement
-                          .css('position', '')
-                          .css('top', '')
-                          .removeClass(s.className);
-                      s.stickyElement.parent().removeClass(s.className);
-                      s.currentTop = null;
-                  }
-              }
-              else {
-                  var newTop = documentHeight - s.stickyElement.outerHeight()
-                      - s.topSpacing - s.bottomSpacing - scrollTop - extra;
-                  if (newTop < 0) {
-                      newTop = newTop + s.topSpacing;
-                  } else {
-                      newTop = s.topSpacing;
-                  }
-                  if (s.currentTop != newTop) {
-                      s.stickyElement
-                          .css('position', 'fixed')
-                          .css('top', newTop)
-                          .addClass(s.className);
-                      s.stickyElement.parent().addClass(s.className);
-                      s.currentTop = newTop;
-                  }
-              }
+      topSpacing: 0,
+      bottomSpacing: 0,
+      className: 'is-sticky',
+      wrapperClassName: 'sticky-wrapper'
+    },
+    $window = $(window),
+    $document = $(document),
+    sticked = [],
+    windowHeight = $window.height(),
+    scroller = function () {
+      var scrollTop = $window.scrollTop(),
+        documentHeight = $document.height(),
+        dwh = documentHeight - windowHeight,
+        extra = (scrollTop > dwh) ? dwh - scrollTop : 0;
+      for (var i = 0; i < sticked.length; i++) {
+        var s = sticked[i],
+          elementTop = s.stickyWrapper.offset().top,
+          etse = elementTop - s.topSpacing - extra;
+        if (scrollTop <= etse) {
+          if (s.currentTop !== null) {
+            s.stickyElement
+              .css('position', '')
+              .css('top', '')
+              .removeClass(s.className);
+            s.stickyElement.parent().removeClass(s.className);
+            s.currentTop = null;
           }
-      },
-      resizer = function() {
-          windowHeight = $window.height();
-      },
-      methods = {
-          init: function(options) {
-              var o = $.extend(defaults, options);
-              return this.each(function() {
-                  var stickyElement = $(this);
+        } else {
+          var newTop = documentHeight - s.stickyElement.outerHeight() -
+            s.topSpacing - s.bottomSpacing - scrollTop - extra;
+          if (newTop < 0) {
+            newTop = newTop + s.topSpacing;
+          } else {
+            newTop = s.topSpacing;
+          }
+          if (s.currentTop != newTop) {
+            s.stickyElement
+              .css('position', 'fixed')
+              .css('top', newTop)
+              .addClass(s.className);
+            s.stickyElement.parent().addClass(s.className);
+            s.currentTop = newTop;
+          }
+        }
+      }
+    },
+    resizer = function () {
+      windowHeight = $window.height();
+    },
+    methods = {
+      init: function (options) {
+        var o = $.extend(defaults, options);
+        return this.each(function () {
+          var stickyElement = $(this);
 
-                  stickyId = stickyElement.attr('id');
-                  var wrapper = $("<div></div>")
-                      .attr('id', stickyId + '-sticky-wrapper')
-                      .addClass(o.wrapperClassName);
-                  stickyElement.wrapAll(wrapper);
-                  var stickyWrapper = stickyElement.parent();
-                  stickyWrapper.css('height', stickyElement.outerHeight());
-                  sticked.push({
-                      topSpacing: o.topSpacing,
-                      bottomSpacing: o.bottomSpacing,
-                      stickyElement: stickyElement,
-                      currentTop: null,
-                      stickyWrapper: stickyWrapper,
-                      className: o.className
-                  });
-              });
-          },
-          update: scroller
-      };
+          stickyId = stickyElement.attr('id');
+          var wrapper = $("<div></div>")
+            .attr('id', stickyId + '-sticky-wrapper')
+            .addClass(o.wrapperClassName);
+          stickyElement.wrapAll(wrapper);
+          var stickyWrapper = stickyElement.parent();
+          stickyWrapper.css('height', stickyElement.outerHeight());
+          sticked.push({
+            topSpacing: o.topSpacing,
+            bottomSpacing: o.bottomSpacing,
+            stickyElement: stickyElement,
+            currentTop: null,
+            stickyWrapper: stickyWrapper,
+            className: o.className
+          });
+        });
+      },
+      update: scroller
+    };
 
   // should be more efficient than using $window.scroll(scroller) and $window.resize(resizer):
   if (window.addEventListener) {
-      window.addEventListener('scroll', scroller, false);
-      window.addEventListener('resize', resizer, false);
+    window.addEventListener('scroll', scroller, false);
+    window.addEventListener('resize', resizer, false);
   } else if (window.attachEvent) {
-      window.attachEvent('onscroll', scroller);
-      window.attachEvent('onresize', resizer);
+    window.attachEvent('onscroll', scroller);
+    window.attachEvent('onresize', resizer);
   }
 
-  $.fn.sticky = function(method) {
-      if (methods[method]) {
-          return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-      } else if (typeof method === 'object' || !method ) {
-          return methods.init.apply( this, arguments );
-      } else {
-          $.error('Method ' + method + ' does not exist on jQuery.sticky');
-      }
+  $.fn.sticky = function (method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.sticky');
+    }
   };
-  $(function() {
-      setTimeout(scroller, 0);
+  $(function () {
+    setTimeout(scroller, 0);
   });
 })(jQuery);
 /////////////////////////////////////////////////
---
+++ wwwroot/js/responsive.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+    responsive();
+});
+$(window).resize(function (e) {
+    responsive();
+    if(!($('html').hasClass('mobile'))){
+        if($(window).width()<768 )
+        {
+            $('html').addClass('mobile').removeClass('desktop');
+            if($(window).width()<320){
+                $('html').removeClass('tablet').addClass('phone');
+            }
+            else{
+                $('html').addClass('tablet').removeClass('phone');
+            }
+        }
+        else{
+            $('html').addClass('desktop').removeClass('mobile').removeClass('tablet').removeClass('phone');
+        }
+    }
+
+    $('.col-map iframe').height($(window).height());
+});
+function responsive() {
+    var md = new MobileDetect(window.navigator.userAgent);
+    //Desktop
+    if (md.phone() === null && md.tablet() === null) {
+        $('html').addClass('desktop').removeClass('mobile').removeClass('tablet').removeClass('phone');
+    }
+    //Mobile
+    else {
+        $('html').addClass('mobile').removeClass('desktop');
+        if (md.phone())
+        $('html').addClass('phone').removeClass('tablet');
+        else
+        $('html').addClass('tablet').removeClass('phone');
+    }
+
+    //browser
+    var ua = navigator.userAgent.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+    var browser='';
+    if (navigator.userAgent.match(/Edge/i) || navigator.userAgent.match(/Trident.*rv[ :]*11\./i)) {
+        browser = "msie";
+    }
+    else {
+        browser = ua[1].toLowerCase();
+        if(browser ==='safari')
+        $('html').addClass('safari');
+        else
+        $('html').removeClass('safari');
+    }
+};
+$(window).scroll(function () {
+    var h =50;
+    if ($(window).scrollTop() >= h) {
+        $('.desktop #header').addClass('fixed');
+    } 
+    else {
+        $('#header').removeClass('fixed');
+    }
+});
